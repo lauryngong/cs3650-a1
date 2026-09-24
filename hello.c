c5b59a1edf79acb5b323d8aede0fb5fa1a512b82
Lauryn Gong <lauryn.gong@gmail.com>
2026-09-24 01:16:19 -0400

    deliverable 2 part 2

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Lauryn\ngong.la@northeastern.edu\n");
+  int i;
+  for (i = 0; i < 20; i++) {
+    printf("Lauryn\ngong.la@northeastern.edu\n");
+  }
 
   return 0;
 }
